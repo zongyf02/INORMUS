187e1b26d52fb3d498ccedecf25e44aab6adb304
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2021-07-24 17:34:24 -0400

    put parse_num into c++
    lets gooo

---
+++ src/helpers_c.cpp
@@ -0,0 +1,33 @@
+#include <Rcpp.h>
+#include <string>
+using namespace Rcpp;
+
+//' Convert string to number
+//'
+//' M will be converted to -2
+//' * (lost) will be converted to -3
+//' N will be converted to -1
+//'
+//' @param x string
+//' @return integer
+//' @export
+//' @import tidyverse
+// [[Rcpp::export]]
+IntegerVector parse_num_c(CharacterVector x) {
+  int l = x.size();
+  IntegerVector result(l);
+  for(int i = 0; i<l; ++i){
+    std::string s;
+    s = x[i];
+    if(s == "M"){
+      result[i] = -2;
+    }else if(s == "*"){
+      result[i] = -3;
+    }else if(s == "N"){
+      result[i] = -1;
+    }else{
+      result[i] = std::stoi(s);
+    }
+  }
+  return result;
+}
